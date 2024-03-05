65ce1b440a23853e1fe28461617f6354062dcd3a
yntymak17 <161040461+yntymak17@users.noreply.github.com>
2024-03-05 20:18:55 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,156 @@
+//   User = prompt('У квадратного стола отпилили один угол. Сколько теперь углов у него стало?');
+ 		// 	if (User === 'five' || User === 'Five' || parseInt(User) === 5) {
+ 		// 		alert('Правильный ответ');
+ 		// 	} else {
+ 		// 		alert('Ответ не верен');
+ 		// 	}
+        //     Задание1==================
+
+
+
+        // User = prompt('Сколько вам лет?');
+       
+		// if (User > 1 && User <=13) {
+		// 	alert('Советую посмотреть "Победитель" ');
+		// }
+		// else if (User > 13 && User <= 17) {
+		// 	alert('Советую посмотреть "Темный Рыцарь"');
+		// }
+		// else if (User > 18 && User <= 100) {
+		// 	alert('Советую посмотреть "Титаник" ');
+		// }
+		// else{
+		// 	alert('Вы не ввели свой возраст')
+		// }
+        // Задание2================
+
+
+
+
+
+        // const drink = prompt('Какой напиток вы предпочитаете?: ?');
+        // switch (drink) {
+        //     case 'Водка':
+        //     case 'водка':
+        //         alert('Вам есть 21?');
+        //         break;
+        //     case 'Пиво':
+        //     case 'пиво':
+        //         alert('Сегодня не выходной');
+        //         break;
+        //     case 'Молоко':
+        //     case 'молоко':
+        //         alert('Довольно полезный напиток');
+        //         break;
+        //     case '':
+        //     case '':
+        //         alert('');
+        //         break;
+        //     case 'Кола':
+        //     case 'кола':
+        //         alert('Вредный,но сладкий ');
+        //         break;
+        //     case 'Вода':
+        //     case 'вода':
+        //         alert('Вы молодец');
+        //         break;
+        //     default:
+        //         alert('Извиняюсь,такого напитка нету')
+        //     }
+        //Задание3===================
+
+
+
+
+
+
+
+
+
+//         let number = 10;
+// if (number = 11) {      //надо поставить не один '='
+//     alert('Number is 11!');
+// } else {
+//     alert('Number is 10!');
+// }
+//Задание4====================
+
+
+
+
+
+
+
+
+
+//  const isRaining = true;
+//  const isSunny = false;
+
+//  const isRainAndSunny = isRaining && isSunny;
+
+//  console.log(isRainAndSunny); 
+//Задание6====================
+
+
+
+
+
+
+
+
+
+
+// let numberAsString = 42;
+
+// console.log(numberAsString + 10);
+//Задание7====================
+
+
+
+
+// let age = 21;
+// if (age >= 18) {
+//     console.log('Вы можете получить права');
+// } else {
+//     console.log('Вы еще не достигли возраста чтобы получать права');
+// }
+//Задание8====================
+
+
+
+
+
+
+
+// const isWeekend = true;
+// const hasTimeOff = true;
+
+// if (isWeekend && hasTimeOff) {
+//     console.log('Вы можете отдохнуть');
+// } else {
+//     console.log('Вы не можете отдохнуть');
+// }
+//Задание9====================
+
+
+
+
+
+
+// const password = "MySecretPassword123";
+// if (password.length >= 8) {
+//     if (/[A-Z]/.test(password)) {
+//         if (/\d/.test(password)) {
+//             console.log("Пароль корректный");
+//         } else {
+//             console.log("Пароль не корректный. Не содержит цифру.");
+//         }
+//     } else {
+//         console.log("Пароль не корректный. Не содержит заглавную букву.");
+//     }
+// } else {
+//     console.log("Пароль не корректный. Длина должна быть не менее 8 символов.");
+// }
+//Задание10====================
+
+        
